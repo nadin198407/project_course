eb626d9991593bd12ceb2d6709ad44f4eae674a8
Nadia Bronnikova <bronnikova@ujp.cz>
2022-08-04 15:45:01 +0200

    adding files project to repo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
 
+import FirstPlot from './SecondPlot';
+
+const data = [
+  {x: 1, y: 4},
+  {x: 2, y: 2},
+  {x: 3, y: 5}
+];
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <FirstPlot data={data}/>
+  
+  
 }
 
 export default App;
